11da43abe0f0bd24409e0e8967284f0658d99a6f
Lint404 <klattkl@gmatc.matc.edu>
2022-02-08 19:13:25 -0600

    Completed Activity 3

--- server.js
+++ server.js
@@ -25,25 +25,20 @@ res.send('http get request sent to root api endpoint')
  * @desc Register user
  */
 
-app.post(
-  "/api/users",
-  [
-    check("name", "Please enter your name").not().isEmpty(),
-    check("email", "Please enter a valid email").isEmail(),
-    check(
-      "password",
-      "Please enter a password with 6 or more characters"
-    ).isLength({ min: 6 }),
-  ],
-  (req, res) => {
+app.post('/api/users',
+[
+    check('name', 'Please enter your name').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
+],
+(req,res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    } else {
-      return res.send(req.body);
+    if (!errors.isEmpty()){
+        return res.status(422).json({errors:errors.array()});
+    } else{
+    return res.send(req.body);
     }
-  }
-);
-
+});
+//
 //Connection listener
 app.listen(3000, () => console.log(`Express server running on port 3000`));
