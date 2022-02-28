fba5a3153ae7c275685b8c8b225508592e6a09ec
Lint404 <klattkl@gmatc.matc.edu>
2022-02-28 14:18:42 -0600

    Added jsonwebtoken and config

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import connectDatabase from './config/db';
 import {check, validationResult} from 'express-validator';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 import User from './models/User';
 
 //Initialize express application
@@ -67,11 +69,28 @@ async (req,res) => {
 
             // Save to the db and return
             await user.save();
-            res.send('User successfully registered');
+            
+            //Generate and return JWT Token
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload, config.get ('jwtSecret'),
+                {
+                    expiresIn: '10hr'
+                },
+                (err,token) => {
+                    if(err) throw err;
+                    res.json({token:token});
+                }
+            );
+
         } catch (error){
             res.status(500).send('Server error');
         }
-        // return res.send(req.body);
     }
 });
 //
